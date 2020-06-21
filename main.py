d9f1d4f8149a489ea5f968dcae54ae5e6bbc9492
artv15 <35376120+artv15@users.noreply.github.com>
2020-06-22 00:30:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ async def on_ready():
 
 #Команда startvote
 @Bot.command(pass_context=True)
-async def startvote(ctx, arg):
-    emb = discord.Embed(title=f'Начато голосование на ивент',
-                        description='Ивент: ' + str(arg),
-                        colour=discord.Color.purple())
+@commands.has_permissions(administrator=True)
+    async def startvote(ctx, arg):
+        emb = discord.Embed(title=f'Начато голосование на ивент',
+                            description='Ивент: ' + str(arg),
+                            colour=discord.Color.purple())
 
-    message = await ctx.send(embed=emb) # Возвращаем сообщение после отправки
-    await message.add_reaction('✅')
-    await message.add_reaction('❌')
-    print('>>Sent message about voting of event. Name of event: ' + str(arg))
+        message = await ctx.send(embed=emb) # Возвращаем сообщение после отправки
+        await message.add_reaction('✅')
+        await message.add_reaction('❌')
+        print('>>Sent message about voting of event. Name of event: ' + str(arg))
 #Конец startvote
 
 
