7028816c4f794732ac366b4061f3f78bb1f8e510
artv15 <35376120+artv15@users.noreply.github.com>
2020-06-24 12:14:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,42 +37,44 @@ async def startvote(ctx, arg):
 #Конец startvote
 
 #Начало endvote
+n = 0
+y = 0
+result = "?"
+
 @Bot.event
-async def on_raw_reaction_add(self, payload):
+async def on_raw_reaction_add(payload):  # №2
     channel = Bot.get_channel(payload.channel_id) # получаем объект канала
     message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
     for emoji in message:
         emoji = payload.emoji
-        if emoji == '✅':
-            global Y
-            Y += 1
-        elif emoji == '❌':
-            global N
-            N += 1
-async def on_raw_reaction_remove(self, payload):
+        if emoji == "\N{WHITE HEAVY CHECK MARK}":
+            y += 1
+        elif emoji == "\N{CROSS MARK}":
+            n += 1
+
+@Bot.event()  # № 1
+async def on_raw_reaction_remove(payload):  # №2
     channel = Bot.get_channel(payload.channel_id) # получаем объект канала
     message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
     for emoji in message:
         emoji = payload.emoji # реакция пользователя
-        if emoji == '✅':
-            global Y
-            Y -= 1
-        elif emoji == '❌':
-            global N
-            N -= 1
-if Y > N:
-    Result = 'Принято'
-elif Y == N:
-    Result = 'Отказано(Да = Нет)'
-else:
-    Resule = 'Отказано'
-@Bot.command(pass_context= True)
+        if emoji == "\N{WHITE HEAVY CHECK MARK}":
+            y -= 1
+        elif emoji == "\N{CROSS MARK}":
+            n -= 1
+
+@Bot.command()  # № 4
 @commands.has_permissions(administrator=True)
 async def endvote(ctx):
-    emb = discord.Embed(title=f'Окончено голосование.', description = 'Результат: ' + str(Result), colour=discord.Color.purple())
-    message = await ctx.send(embed=emb) # Возвращаем сообщение после отправки
-Y = 0
-N = 0
+    if y > n:
+        result = 'Принято'  # № 5
+    elif y == n:
+        result = 'Отказано (Да = Нет)'  # № 5
+    else:
+        result = 'Отказано'  # № 5
+    emb = discord.Embed(title=f'Окончено голосование.', description = 'Результат: ' + result, colour=discord.Color.purple())
+    return await ctx.send(embed=emb) # **Возвращаем** сообщение после отправки.
+    y, n = 0, 0
 #Конец endvote
 
 
