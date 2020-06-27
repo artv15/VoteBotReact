19de11b8ccf567a4db8ca801e7f99ea2d07449db
artv15 <35376120+artv15@users.noreply.github.com>
2020-06-27 09:05:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,64 +28,34 @@ async def on_ready():
 @commands.has_permissions(administrator=True)
 async def startvote(ctx, arg):
     emb = discord.Embed(title=f'Начато голосование',
-                        description='Голосуем за: ' + str(arg),
+                        description='Голосуем за: ' + str(arg) + '\n' + 'Голосование кончается через 30 секунд',
                         colour=discord.Color.purple())
 
     message = await ctx.send(embed=emb) # Возвращаем сообщение после отправки
     await message.add_reaction('✅')
     await message.add_reaction('❌')
     print('>>Sent message about voting. Voting for: ' + str(arg))
+    @Bot.event()
+    @Bot.on_reaction_add()
+    emojis = message.get_all_emojis()
+    for i in emojis:
+        if i == '✅':
+            y += 1
+        elif i == '❌'
+            n += 1
+    @Bot.event()
+    @Bot.on_reaction_remove()
+    emojis = message.get_all_emojis()
+    for i in emojis:
+        if i == '✅':
+            y += 1
+        elif i == '❌'
+            n += 1
 #Конец startvote
 
 
 #Начало endvote
-@Bot.command  # № 4
-@commands.has_permissions(administrator=True)
-@Bot.event
 
-async def on_raw_reaction_add(payload):  # №2
-    channel = Bot.get_channel(payload.channel_id) # получаем объект канала
-    message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
-    emoji = payload.emoji
-    if I_WAS_TESTING == 0:
-        y = 0
-        n = 0
-        I_WAS_TESTING = 1
-    if emoji == "\N{WHITE HEAVY CHECK MARK}":
-        y += 1
-    elif emoji == "\N{CROSS MARK}":
-        n += 1
-    return()
-
-@Bot.event  # № 1
-async def on_raw_reaction_remove(payload):  # №2
-    channel = Bot.get_channel(payload.channel_id) # получаем объект канала
-    message = await channel.fetch_message(payload.message_id) # получаем объект сообщения
-    emoji = payload.emoji # реакция пользователя
-    if I_WAS_TESTING == 0:
-        y = 0
-        n = 0
-        I_WAS_TESTING = 1
-    if emoji == "\N{WHITE HEAVY CHECK MARK}":
-        y -= 1
-    elif emoji == "\N{CROSS MARK}":
-        n -= 1
-    return()
-
-@Bot.command
-async def endvote(ctx):
-    y = y
-    n = n
-    if y > n:
-        result = 'Принято'  # Voting_ACCEPTED
-    elif y == n:
-        result = 'Отказано (Да = Нет)'  # Voting_REFUSED (y=n)
-    else:
-        result = 'Отказано'  # Voting_REFUSED
-    emb = discord.Embed(title=f'Окончено голосование.', description = 'Результат: ' + result, colour=discord.Color.purple())
-    return await ctx.send(embed=emb) # **Возвращаем** сообщение после отправки.
-    y = 0
-    n = 0
 #Конец endvote
 
 #Команда debug
